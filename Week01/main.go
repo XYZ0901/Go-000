204310113cb259643166effc660b5791b8a2f01c
xyz2zyx <zh_vaeloveyou@qq.com>
2020-11-22 17:40:10 +0800

    section 1 update function Subset

--- Week01/main.go
+++ Week01/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println("hello,world!")
+}
+
+// service
+type service struct{}
+
+// Subset 子集算法 backends: 服务节点  clientId: 客户端id  subsetSize: 子集大小
+func Subset(backends []int, clientId, subsetSize int) []int {
+	subsetCount := len(backends) / subsetSize
+
+	round := clientId / subsetCount
+	rand.Seed(int64(round))
+	rand.Shuffle(len(backends), func(i, j int) {
+		backends[i], backends[j] = backends[j], backends[i]
+	})
+
+	subsetId := clientId % subsetCount
+
+	start := subsetId * subsetSize
+	return backends[start : start+subsetSize]
 }
