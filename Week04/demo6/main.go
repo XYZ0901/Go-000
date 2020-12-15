fe4840a2e2d36751d081a6ce5a162ee34b2916cd
xyz2zyx <zh_vaeloveyou@qq.com>
2020-12-15 14:54:46 +0800

    UPDATE 4 WEEK

--- Week04/demo6/main.go
+++ Week04/demo6/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "time"
 
+type Option func(o *options)
+
+type options struct {
+	stopTimeout time.Duration
+}
+
+func StopTimeout(d time.Duration) Option {
+	return func(o *options) {
+		o.stopTimeout = d
+	}
+}
+
+func main() {
+	o := options{}
+	f := StopTimeout(6)
+	f(&o)
 }
